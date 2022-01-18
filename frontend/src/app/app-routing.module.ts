da6aff19824f2c62ce56bab27fd300a15fd9b3a7
madv1ll <kevint.mtn@gmail.com>
2022-01-18 20:04:58 -0300

    se aregan routerLinks al navBar

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { DestinatariosComponent } from './components/destinatarios/destinatarios.component';
 import { HistorialComponent } from './components/historial/historial.component';
 import { TransferenciaComponent } from './components/transferencia/transferencia.component';
 
 const routes: Routes = [
   { 
+    path: '', component: AppComponent
+  },
+  { 
     path: 'historial', component: HistorialComponent
   },
   { 
     path: 'transferencia', component: TransferenciaComponent
-  }
+  },
+  { 
+    path: 'nuevo', component: DestinatariosComponent
+  },
 ];
 
 @NgModule({
